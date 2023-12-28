b30c4a7614629a7d7419883447784a7f9c1945eb
Bruno Alencar <joaobrunoalencar@gmail.com>
2023-12-28 13:56:03 -0300

    the syntax to set a type with string is using parenteses

--- cards/deck.go
+++ cards/deck.go
@@ -5,13 +5,15 @@ import "fmt"
 // create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+type hand string
 
 func newDeck() deck {
 	cards := deck{}
 	
+	hand1 := hand("Five of Diamonds")
+	fmt.Println(hand1)
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
-	
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value + " of " + suit)
--- cards/main.go
+++ cards/main.go
@@ -2,7 +2,9 @@ package main
 
 func main() {
 	cards := newDeck()
-	
+	hand, cards := deal(cards, 5)
+
+	hand.print()
 	cards.print()
 }
 
