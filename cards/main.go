aea3df04ffae7b5545a3c6cffdcf262c12b27e38
Bruno Alencar <joaobrunoalencar@gmail.com>
2023-12-28 10:47:31 -0300

    two way of declaration/asign of variable, but only one way works outside a function

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,14 @@
 package main
 
 import "fmt"
-
+var number2 int = 2 // this work outside of a function
+number1 := 1 // this doesnt work outside of a function 
 
 func main() {
 	// var card string = "Ace of Spades"
 	card:= "Ace of Spades"
 
 	card = "Five of Diamonds"
-	
+	fmt.Println(number1)
 	fmt.Println(card)		
 }
